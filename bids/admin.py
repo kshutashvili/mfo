a7ad927831ff36167ff33a5c367078c4803a2ac8
anton <phonxis@gmail.com>
2018-07-13 11:03:05 +0300

    Change list_display of Bid model

--- bids/admin.py
+++ bids/admin.py
@@ -4,4 +4,11 @@ from .models import Bid
 
 @admin.register(Bid)
 class BidAdmin(admin.ModelAdmin):
-    list_display = ('contact_phone', 'city', 'wm_id', 'any_param')
+    list_display = (
+        'contact_phone',
+        'city',
+        'wm_id',
+        'any_param',
+        'created_dt',
+        'updated_dt'
+    )
