d12ae1d0e57fee27b7f12d8617f970986f1a2b14
anton <phonxis@gmail.com>
2018-11-22 10:14:20 +0200

    Create initial test for bankid

---
+++ bankid/tests/test_views.py
@@ -0,0 +1,11 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class BankidGetdataView(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test1(self):
+        pass
